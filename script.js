fb646d1c04ec3baa02026902f7e906b4bff69ac1
Leandro <leholiveira14@gmail.com>
2025-04-27 20:42:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem() {
+    const mensagem = document.getElementById('mensagem');
+    mensagem.innerText = "Hello world! Bem-vindo(a) à minha página! Vamos juntos em busca de evolução! 🚀";
+    mensagem.classList.add('mensagem-destaque');
+}
